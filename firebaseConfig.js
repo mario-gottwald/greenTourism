79c1a821a87f6984b384623840a8d08f76745e5f
mario-gottwald <153294552+mario-gottwald@users.noreply.github.com>
2025-07-13 16:53:35 +0200

    empty firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCROPCeXPMXZxvy8BYbXztslrYp1z6XAt4",
-  authDomain: "greentourismo-a82df.firebaseapp.com",
-  projectId: "greentourismo-a82df",
-  storageBucket: "greentourismo-a82df.firebasestorage.app",
-  messagingSenderId: "473150382792",
-  appId: "1:473150382792:web:b03266983f36a9a9e2bbfd"
-};
-
-const FIREBASE_APP = initializeApp(firebaseConfig);
-
-export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)  // AsyncStorage für persistente Authentifizierung verwenden
-});
